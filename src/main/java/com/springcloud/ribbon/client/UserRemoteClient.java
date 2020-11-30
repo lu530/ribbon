1456f13893f9d6f5d687ca229b68e482e46c086b
zhongzhicheng <zhongzhicheng@pcitech.com>
2020-11-30 14:03:25 +0800

    Feign调用demo

--- src/main/java/com/springcloud/ribbon/client/UserRemoteClient.java
+++ src/main/java/com/springcloud/ribbon/client/UserRemoteClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@FeignClient(value = "center-educational-server", url = "http://localhost:8182")
+@FeignClient(value = "center-educational-server", url = "${yitu.service.url}")
 public interface UserRemoteClient {
 
     @GetMapping("/house/hollo")
